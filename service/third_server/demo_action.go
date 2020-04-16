b8568fc1aa5647d20047f00c7992dbfab3a3e5bc
guyu <guyu29@126.com>
2020-04-16 10:29:59 +0800

    fix: debug

--- service/third_server/demo_action.go
+++ service/third_server/demo_action.go
@@ -9,47 +9,47 @@ type DemoAction struct {
 	mod_base.BaseAction
 }
 
-func(b *DemoAction) GetActionName() string {
+func (b *DemoAction) GetActionName() string {
 	return "DemoAction"
 }
 
-func(b *DemoAction) GetFrontStruct() []*mod_base.FormData {
+func (b *DemoAction) GetFrontStruct() []*mod_base.FormData {
 	return []*mod_base.FormData{
 		{
-			Title:"单选",
-			Key: constant.ItemSingle,
-			Value: "",
-			Position:"0",
-			Options: []string{"A", "B", "C"},
+			Title:    "单选",
+			Key:      constant.ItemSingle,
+			Value:    "",
+			Position: "0",
+			Options:  []string{"A", "B", "C"},
 		},
 		{
-				Title:"多选",
-				Key:constant.ItemCheckbox,
-				Value:"",
-			Position:"1",
-				Options:[]string{"A", "B", "C"},
+			Title:    "多选",
+			Key:      constant.ItemCheckbox,
+			Value:    "",
+			Position: "1",
+			Options:  []string{"A", "B", "C"},
 		},
 		{
-					Title:"文本",
-					Key:constant.ItemText,
-					Value:"",
-			Position:"2",
+			Title:    "文本",
+			Key:      constant.ItemText,
+			Value:    "",
+			Position: "2",
 		},
 		{
-						Title:"时间选择器",
-				Key:constant.ItemDateTime,
-				Value:"",
-			Position:"3",
+			Title:    "时间选择器",
+			Key:      constant.ItemDateTime,
+			Value:    "",
+			Position: "3",
 		},
 		{
-							Title:"多行文本",
-			Key:constant.ItemMutiText,
-			Value:"",
-			Position:"4",
+			Title:    "多行文本",
+			Key:      constant.ItemMutiText,
+			Value:    "",
+			Position: "4",
 		},
 	}
 }
 
-func(b *DemoAction) ExecAction() error {
+func (b *DemoAction) ExecAction() error {
 	return nil
 }
